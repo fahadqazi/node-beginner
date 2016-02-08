16a9a974fee3797cc561cdf6b89f0050f57d712e
Fahad Qazi <fahadqazi@gmail.com>
2016-02-08 13:37:13 +0000

    modified code to handle post request from start page and view on upload page

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,30 @@
-var exec = require("child_process").exec;
+var querystring = require('querystring');
 
-function start(res){
+function start(res, postData){
   console.log("Request handler 'start' was called");
 
-  exec("ls -lah", function(err,stdout,stderr){
-    res.writeHead(200, {"Content-Type":"text/plain"});
-    res.write(stdout);
+  var body =
+'<html>'+
+'<head>'+
+'<meta http-equiv="Content-Type" content="text/html; '+ 'charset=UTF-8" />'+
+'</head>'+
+'<body>'+
+'<form action="/upload" method="post">'+
+'<textarea name="text" rows="20" cols="60"></textarea>'+
+'<input type="submit" value="Submit text" />'+ '</form>'+
+'</body>'+
+'</html>';
+//  exec("ls -lah", function(err,stdout,stderr){
+    res.writeHead(200, {"Content-Type":"text/html"});
+    res.write(body);
     res.end();
-  });
+
 }
 
-function upload(res){
+function upload(res, postData){
   console.log("Request hander 'upload' was called");
   res.writeHead(200, {"Content-Type":"text/plain"});
-  res.write("Hello Uploads!");
+  res.write("You've sent: " + querystring.parse(postData).text);
   res.end();
 }
 
